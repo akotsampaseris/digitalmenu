4081dea7c69e25b4034acefa94d7f9d77c5ad2f5
thenegativeentropy <a.kotsampaseris@gmail.com>
2020-06-21 00:37:50 +0100

    Restructure

--- README.md
+++ README.md
@@ -2,5 +2,14 @@
 
 DigitalMenu is an online hub that stores and displays digital menus for restaurants and cafes.
 
+## Technologies
+
+DigitalMenu is a modern web application written in Django, GraphQL (via Django Graphene), Apollo and React.
+
 ## Author
+
 [theNegativeEntropy](https://thenegativeentropy.org)
+
+
+## License
+[MIT](https://thenegativeentropy.org)
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,1 +1,55 @@
-import App from "./components/App";
+import React, { Component } from "react";
+import { render } from "react-dom";
+
+import ApolloClient from "apollo-boost";
+import { ApolloProvider, useMutation } from "@apollo/react-hooks";
+import gql from "graphql-tag";
+
+const client = new ApolloClient({
+  uri: 'http://localhost:8000/api/',
+});
+
+const CREATE_SHOP = gql`
+mutation CreateShop($slug: String!){
+  createShop(slug: $slug){
+      slug
+  }
+}`;
+
+function CreateShop() {
+  let input;
+  const [createShop, { data }] = useMutation(CREATE_SHOP);
+
+  return (
+    <div>
+      <form
+        onSubmit={e => {
+          e.preventDefault();
+          createShop({ variables: { slug: input.value } });
+          input.value = '';
+        }}
+      >
+        <input className="input form-input"
+          ref={node => {
+            input = node;
+          }}
+        />
+        <button className="btn btn-primary" type="submit">Create Shop</button>
+      </form>
+    </div>
+  );
+}
+
+
+class App extends Component {
+  render() {
+    return (
+    <ApolloProvider client={client}>
+      <h2>My first apollo app</h2>
+      <CreateShop />
+    </ApolloProvider>
+    );
+  }
+}
+
+render(<App />, document.getElementById("app"));
